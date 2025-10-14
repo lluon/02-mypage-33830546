12f91b344a41d2a7ea7e44be2dec8a6acb29fd10
lluon <lluon001@gold.ac.uk>
2025-10-14 15:54:13 +0100

    update comment and readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# lucio luongo's web application
+### version 1.0
+very small node.js web application.
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var http = require("http"); 
-const port = 8000; 
-
+var http = require("http"); // Import the built-in 'http' module to create an HTTP server
+const port = 8000; // Define the port number the server will listen on
+""
 http.createServer(function(req, res) { 
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello World!");
